1cddc80879aa2befbf668ba5f74baf935958e277
xzaz <e_spoel@hotmail.com>
2020-06-12 15:55:13 +0200

    add rf

--- RPI-433-MQTT/rpi-433-mqtt.py
+++ RPI-433-MQTT/rpi-433-mqtt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import sys
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import configparser
@@ -9,18 +8,35 @@ from rpi_rf import RFDevice
 def on_connect(client, userdata, flags, rc):
     print("Connected with code: " + str(rc))
 
-def main(argv):
+def main():
+    #Config
     config = configparser.ConfigParser()
     config.read('config.ini')
     cMqtt = config['MQTT']
+    cGPIO = config['GPIO']
+    
+    #MQTT
     client = mqtt.Client()
     client.on_connect = on_connect
     ##client.on_message = on_message
     client.username_pw_set(cMqtt['username'], cMqtt['password'])
     client.connect(cMqtt['host'])
     client.loop_start()
+
+    #RF
+    rf = RFDevice(cGPIO['pin'])
+    rf.enable_rx()
+    rf.rx_callback
+
+    timestamp = None
+    while True:
+        if rf.rx_code_timestamp != timestamp:
+            timestamp = rf.rx_code_timestamp
+            print(str(rf.rx_code))
+        
+
     input("Press any key to exit..\n")
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
