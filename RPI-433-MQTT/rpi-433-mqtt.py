83f0223ad48c01c72886efa4dde62e9c95e20dff
xzaz <e_spoel@hotmail.com>
2020-06-12 16:59:36 +0200

    remove rf

--- RPI-433-MQTT/rpi-433-mqtt.py
+++ RPI-433-MQTT/rpi-433-mqtt.py
@@ -3,7 +3,34 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import configparser
-from rpi_rf import RFDevice
+import time
+import warnings
+
+try:
+    import RPi.GPIO as GPIO
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+except RuntimeError:
+    warnings.warn("This can only be run on a Raspberry PI", RuntimeWarning)
+
+def read_timings(rx_pin):
+    capture = []
+    while True:
+        start = time.time()
+        if GPIO.wait_for_edge(rx_pin, GPIO.BOTH, timeout=1000):
+            capture.append((time.time() - start, GPIO.input(rx_pin)))
+
+        elif len(capture) < 5:  # Any pattern is likely larger than 5 bits
+            capture = []
+        else:
+            return capture
+
+
+def record(rxpin):
+    GPIO.setup(rxpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    sample = read_timings(rxpin)
+    print('Recorded', len(sample), 'bit transitions')
+    return sample
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with code: " + str(rc))
@@ -23,18 +50,7 @@ def main():
     client.connect(cMqtt['host'])
     client.loop_start()
 
-    #RF
-    rf = RFDevice(int(cGPIO['pin']))
-    rf.enable_rx()
-    rf.rx_callback
-
-    timestamp = None
-    while True:
-        if rf.rx_code_timestamp != timestamp:
-            timestamp = rf.rx_code_timestamp
-            print(str(rf.rx_code))
-        
-
+    record(int(cGPIO['pin']))
     input("Press any key to exit..\n")
 
 
