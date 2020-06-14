62b743601c8c147010012a73357ecdf128809010
xzaz <e_spoel@hotmail.com>
2020-06-14 15:49:15 +0200

    cmd?

--- RPI-433-MQTT/rpi-433-mqtt.py
+++ RPI-433-MQTT/rpi-433-mqtt.py
@@ -50,7 +50,7 @@ def main():
     devices = cDevices['devices'].split(';')
 
     for device in devices:
-        client.publish("homeassistant/switch/rpi-433-mqtt/" + str(device) + "/config", json.dumps({"name" : str(device), "command_topic" : "homeassistant/switch/rpi-433-mqtt/" + str(device) + "/set", "state_topic" : "homeassistant/switch/rpi-433-mqtt/" + str(device) + "/state", "payload_on" : "ON", "payload_off" : "OFF", "optimistic" : False }))
+        client.publish("homeassistant/switch/rpi-433-mqtt/" + str(device) + "/config", json.dumps({"name" : str(device), "command_topic" : "homeassistant/switch/rpi-433-mqtt/" + str(device) + "/cmd", "state_topic" : "homeassistant/switch/rpi-433-mqtt/" + str(device) + "/state", "payload_on" : "ON", "payload_off" : "OFF", "optimistic" : False }))
 
     while True:
         if rfdevice.rx_code_timestamp != timestamp:
